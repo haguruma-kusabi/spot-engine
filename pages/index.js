6b950431c9e620ba86ae73342a9bf3e784ff230c
haguruma-kusabi <ksn.111324212@gmail.com>
2026-05-15 12:58:08 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        padding: 40,
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>SPOT SERIES</h1>
+
+      <ul>
+        <li>
+          <a href="/choco">
+            CHOCO 🌿 SPOT
+          </a>
+        </li>
+
+        <li>
+          <a href="/matcha">
+            METCHA 🍵 MATCHA
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
