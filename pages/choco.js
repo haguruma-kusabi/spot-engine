d9bb85b4771b85bcb9441288cf83ab617224c27c
haguruma-kusabi <ksn.111324212@gmail.com>
2026-05-15 14:40:37 +0900

    Delete pages directory

--- pages/choco.js
+++
@@ -1,7 +0,0 @@
-import HomePage from "../src/HomePage";
-
-import { theme } from "../themes/choco";
-
-export default function Page() {
-  return <HomePage theme={theme} />;
-}
--- pages/matcha.js
+++
@@ -1,7 +0,0 @@
-import HomePage from "../src/HomePage";
-
-import { theme } from "../themes/matcha";
-
-export default function Page() {
-  return <HomePage theme={theme} />;
-}
