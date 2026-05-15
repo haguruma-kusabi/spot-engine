6a1026c1dc0513ad6ed5e87c7c1a3cfa19c56c1f
haguruma-kusabi <ksn.111324212@gmail.com>
2026-05-15 12:54:57 +0900

    Create matcha.js

---
+++ themes/matcha.js
@@ -0,0 +1,19 @@
+export const theme = {
+  id: "matcha",
+
+  title: "METCHA 🍵 MATCHA",
+
+  background:
+    "linear-gradient(180deg,#0f3d2e,#1f5c45,#3b7a57)",
+
+  primary: "#27ae60",
+
+  keywords: [
+    "抹茶",
+    "宇治抹茶",
+    "抹茶ラテ",
+    "抹茶スイーツ",
+  ],
+
+  emoji: "🍵",
+};
