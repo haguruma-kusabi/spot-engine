7400cbf94509500dce0f3a03c5ef0aa8a8ff442a
haguruma-kusabi <ksn.111324212@gmail.com>
2026-05-15 12:54:27 +0900

    Create choco.js

---
+++ themes/choco.js
@@ -0,0 +1,18 @@
+export const theme = {
+  id: "choco",
+
+  title: "CHOCO 🌿 SPOT",
+
+  background:
+    "linear-gradient(180deg,#0f2027,#203a43,#2c5364)",
+
+  primary: "#00c6ff",
+
+  keywords: [
+    "チョコミント",
+    "チョコミントアイス",
+    "チョコミント新商品",
+  ],
+
+  emoji: "🍫",
+};
