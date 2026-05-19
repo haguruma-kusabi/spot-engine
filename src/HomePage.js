0aa880d84cc0daa71a19a2ff04e83f0dfc82eb92
haguruma-kusabi <ksn.111324212@gmail.com>
2026-05-19 12:43:25 +0900

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,7 @@ import { useEffect, useMemo, useState } from "react";
 import Header from "./components/Header";
 import FilterBar from "./components/FilterBar";
 import NewsCard from "./components/NewsCard";
-import { detectBrand } from "./lib/brand";
+import { detectBrand } from "../lib/brand";
 
 export default function HomePage({ theme }) {
   const [items, setItems] = useState([]);
