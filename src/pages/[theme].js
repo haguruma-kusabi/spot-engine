b90d5370cdc0800efcb44a72a95fb712a2460b21
haguruma-kusabi <ksn.111324212@gmail.com>
2026-05-19 09:58:53 +0900

    Update [theme].js

--- src/pages/[theme].js
+++ src/pages/[theme].js
@@ -1,30 +1,16 @@
 import HomePage from "../HomePage";
-
 import { themes } from "../themes";
 
-export default function ThemePage({
-  theme,
-}) {
-  return (
-    <HomePage theme={theme} />
-  );
+export default function ThemePage({ theme }) {
+  return <HomePage theme={theme} />;
 }
 
-export async function getServerSideProps({
-  params,
-}) {
-  const theme =
-    themes[params.theme];
+export async function getServerSideProps({ params }) {
+  const theme = themes[params.theme];
 
-  if (!theme) {
-    return {
-      notFound: true,
-    };
-  }
+  if (!theme) return { notFound: true };
 
   return {
-    props: {
-      theme,
-    },
+    props: { theme },
   };
 }
