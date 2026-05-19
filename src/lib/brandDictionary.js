9c3c300749392cd8575e1d8636f0a704fdad701d
haguruma-kusabi <ksn.111324212@gmail.com>
2026-05-19 11:05:19 +0900

    Create brandDictionary.js

---
+++ src/lib/brandDictionary.js
@@ -0,0 +1,78 @@
+export const BRAND_DICTIONARY = [
+  // =================
+  // コンビニ
+  // =================
+  {
+    group: "convenience",
+    name: "セブン",
+    color: "#f15a24",
+    keywords: [
+      "セブン",
+      "セブンイレブン",
+      "セブン-イレブン",
+      "7-ELEVEN",
+      "7-Eleven",
+      "7 eleven",
+      "seven eleven",
+    ],
+  },
+  {
+    group: "convenience",
+    name: "ファミマ",
+    color: "#00a040",
+    keywords: [
+      "ファミマ",
+      "ファミリーマート",
+      "FamilyMart",
+      "familymart",
+      "family mart",
+    ],
+  },
+  {
+    group: "convenience",
+    name: "ローソン",
+    color: "#005bac",
+    keywords: [
+      "ローソン",
+      "LAWSON",
+      "lawson",
+    ],
+  },
+
+  // =================
+  // カフェ
+  // =================
+  {
+    group: "cafe",
+    name: "スタバ",
+    color: "#006241",
+    keywords: [
+      "スタバ",
+      "スターバックス",
+      "Starbucks",
+      "starbucks",
+    ],
+  },
+  {
+    group: "cafe",
+    name: "タリーズ",
+    color: "#000000",
+    keywords: [
+      "タリーズ",
+      "TULLY'S",
+      "Tully's",
+      "tullys",
+    ],
+  },
+  {
+    group: "cafe",
+    name: "ドトール",
+    color: "#f2c200",
+    keywords: [
+      "ドトール",
+      "DOUTOR",
+      "Doutor",
+      "doutor",
+    ],
+  },
+];
